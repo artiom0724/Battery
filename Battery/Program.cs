9d4f953427e09c0d66ae667282367eeb57953d81
artiom0724 <artiom0724@google.com>
2017-10-12 13:52:41 +0300

    Final

--- Battery/Program.cs
+++ Battery/Program.cs
@@ -16,7 +16,7 @@ namespace Battery
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormBattery());
         }
     }
 }
